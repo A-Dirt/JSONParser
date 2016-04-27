2c2c8fb4d2346941bf8c4543b5c0dd8c84928fba
zhangwenwei <a_dirt@icloud.com>
2016-04-27 11:41:54 +0800

    NSObject+JSONExtension

---
+++ NSObject+JSONExtension.h
@@ -0,0 +1,15 @@
+//
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (JSONExtension)
+
+- (void)setDict:(NSDictionary *)dict;
++ (instancetype )objectWithDict:(NSDictionary *)dict;
+
+// 告诉数组中都是什么类型的模型对象 子类中需要重写  return @"数组中元素的类型字符串";
+- (NSString *)arrayObjectClass ;
+
+@end
